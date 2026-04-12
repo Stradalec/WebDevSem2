ab261b6629d258d52a4dedb4dd58e2a5aef5b28a
Stradalec <pochtatipo6@gmail.com>
2026-04-12 20:31:37 +0700

    fix: finally beat .gitignore

---
+++ main_service/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Inject, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), 
+    MongooseModule.forRootAsync({ 
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => ({ 
+        uri: configService.get<string>('MONGO_URL') }) 
+      })], 
+  controllers: [AppController],
+  providers: [AppService],
+
+})
+export class AppModule { }
